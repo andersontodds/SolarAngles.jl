d6e313163b854bdd5edf4b43b3d9e6c4be680a18
Todd Anderson <andersontodds@gmail.com>
2023-02-26 10:59:06 -0800

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using SolarAngles
-using Test
+using Test, Dates
 
 @testset "SolarAngles.jl" begin
-    # Write your tests here.
+    @test SolarAngles.localsolartime(DateTime(2020, 01, 01,00,00,00), 0, 0) <= 0
+    # more tests
 end
